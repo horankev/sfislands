8d065546f0ccf54db7198fcc3be71d3f7e7d092e
horankev <horank@gmail.com>
2024-02-06 20:23:38 +0000

    document the data

--- R/uk_election.R
+++ R/uk_election.R
@@ -1,15 +1,24 @@
 #' UK election data
 #'
-#' A subset of data from the World Health Organization Global Tuberculosis
-#' Report ...
+#' Swing and socio-economic data for England, Scotland & Wales
+#' Census and voting data sourced from parlitools R package
+#' Spatial data sourced from UK government geoportal
 #'
 #' @format ## `uk_election`
-#' A data frame with 7,240 rows and 60 columns:
+#'    An sf and data.frame object with 632 rows and 9 columns
+#'
 #' \describe{
-#'   \item{country}{Country name}
-#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
-#'   \item{year}{Year}
+#'   \item{degree_educated}{percent of population with level 4 qualifications or higher}
+#'   \item{health_not_good}{percent of population reporting health to be fair, bad, or very bad}
+#'   \item{white}{percent of population of exclusively white ethnicity}
+#'   \item{con_swing}{Butler swing to the Conservative Party from the Labour Party from election 2019 to election 2019}
+#'   \item{population}{Constituency population}
+#'   \item{region}{Region}
+#'   \item{county}{County}
+#'   \item{constituency_name}{Constituency}
+#'   \item{geometry}{sfc polygons column}
 #'   ...
 #' }
-#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+#'
+#' @source <https://geoportal.statistics.gov.uk/datasets/ons::wpc-dec-2019-ultra-generalised-clipped-boundaries-uk>, <https://docs.evanodell.com/parlitools/>
 "uk_election"
